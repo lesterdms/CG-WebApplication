90f366b489d73167c3c3061e5ddbd77b373ab9e6
Lester Dave Salazar <69951564+lesterdms@users.noreply.github.com>
2022-12-01 18:55:40 +0800

    Delete CG-Webapp-main directory

--- CG-Webapp-main/includes/footer.php
+++
@@ -1,6 +0,0 @@
-<!--JS -->
-<script src="assets/js/jquery-3.6.1.min.js"></script>
-    <script src="assets/js/bootstrap.bundle.min.js"></script>
-  
-</body>
-</html>
--- CG-Webapp-main/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-if(isset($_SESSION['auth'])){
-    unset($_SESSION['auth']);
-    unset($_SESSION['auth_user']);
-    $_SESSION['message'] = "Logged Out Successfully";
-}
-header('Location: index.php');
-?>
